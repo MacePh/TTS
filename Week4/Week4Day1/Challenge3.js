aa6c75cdec8dfc7bdeab6c2af458a34d0a958ec1
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-08 11:50:32 -0500

    hw

---
+++ Week4/Week4Day1/Challenge3.js
@@ -0,0 +1,56 @@
+// CHALLENGE 3//
+// Going back to our slideshow object, let's add some functionality.
+
+// Create an empty property named playInterval
+
+// A play() function that moves to the next photo ever 2000ms until the end.
+
+// Tip - use playInterval = setInterval(fn,ms).
+
+// A pause() function that stops the slideshow
+
+//  Tip - use clearInterval(playInterval)
+
+// Automatically pause the slideshow if it gets to the end of the photolist while playing.
+
+let slideshow = {
+    photoList: ['birds', 'puppies', 'rainbows', 'kittens', 'babies'],
+
+    currentPhotoIndex: 0,
+
+    nextPhoto: function() {
+        if(this.currentPhotoIndex < this.photoList.length - 1) {
+            this.currentPhotoIndex++;
+            console.log('currentPhoto is: '+ this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('End of Slideshow');
+              this.pause();
+        }
+    },
+
+    prevPhoto: function() {
+        if(this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            console.log('currentPhoto is: ' + this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('Start of Slideshow');
+            
+        }
+    },
+
+    getCurrentPhoto: function() {
+        return this.photoList[this.currentPhotoIndex];  
+    },
+
+    playInterval: null,
+
+    play: function() {
+        let self = this;
+        this.playInterval = setInterval(function(){self.nextPhoto()}, 2000)
+    },
+
+    pause: function() {
+        clearInterval(this.playInterval);
+    }
+
+}
