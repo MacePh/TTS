dc5104f9065f81350d026ab98af23f4a7819a7fc
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-14 18:51:35 -0500

    update

--- Week5/Week5Day1/ToDoList/todo.js
+++ Week5/Week5Day1/ToDoList/todo.js
@@ -1,2 +1,7 @@
-const addItem = document.getElementById("add-item");
-const addTaskButton = document.getElementById("add-task-button");
+const addItem = document.querySelector('ul');
+let
+let button = document.querySelector('button');
+
+button.addEventListener('click',function(event) {
+let 
+})
