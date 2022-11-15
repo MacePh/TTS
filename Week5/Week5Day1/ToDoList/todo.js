e15aca45b14ac60f616e25735379301e8f42a243
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-14 22:29:47 -0500

    update

--- Week5/Week5Day1/ToDoList/todo.js
+++ Week5/Week5Day1/ToDoList/todo.js
@@ -1,7 +1,7 @@
-const addItem = document.querySelector('ul');
-let
-let button = document.querySelector('button');
+const parentUL = document.querySelector('ul');
+let button = document.querySelector("submit");
 
-button.addEventListener('click',function(event) {
-let 
-})
+button.addEventListener('click', function(event) {
+	let newLI = document.createElement('li');
+    newLI.innerHTML=document.getElementById('newToDo');
+    parentUL.appendChild(newLI)});
