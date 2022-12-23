fd9247e37a1c3ba397cc1b22bbaa4847b23ffd5c
MacePh <115651516+MacePh@users.noreply.github.com>
2022-12-23 11:39:29 -0500

    Spring Boot Project 1

---
+++ Week11/SpringBootProject1/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostRepository.java
@@ -0,0 +1,5 @@
+package com.techtalentsouth.TechTalentBlog.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{}
