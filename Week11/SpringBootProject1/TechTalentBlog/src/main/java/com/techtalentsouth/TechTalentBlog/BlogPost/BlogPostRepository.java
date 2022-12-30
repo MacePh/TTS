4da2b1dbfc46fd4b35e5b4b2480263b07db83a57
MacePh <115651516+MacePh@users.noreply.github.com>
2022-12-30 02:57:37 -0500

    blog post uploaded

--- Week11/SpringBootProject1/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostRepository.java
+++ Week11/SpringBootProject1/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostRepository.java
@@ -1,5 +1,7 @@
 package com.techtalentsouth.TechTalentBlog.BlogPost;
 
 import org.springframework.data.repository.CrudRepository;
+import java.math.BigInteger;
 
-public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{}
+public interface BlogPostRepository extends CrudRepository<BlogPost, BigInteger>{
+}
